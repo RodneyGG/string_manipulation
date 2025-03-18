af563a078a22d739db787538c4e2fbf854855d3c
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-19 07:49:11 +0800

    pseudocode

---
+++ print_all_caps/print_all_caps.py
@@ -0,0 +1,11 @@
+"""
+Prog03: Create a program that ask the user to input their fullname. 
+Print the input in all capital letter.
+Example:
+Input: Juan Dela Cruz
+Output: JUAN DELA CRUZ
+"""
+
+#Ask the user to input a name
+#Convert name to all uppercase
+#print name
