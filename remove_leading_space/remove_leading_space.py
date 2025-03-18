96c939e1c26bc9faacd4f31a980a15e6627930e6
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-19 00:21:43 +0800

    ensure name validity

--- remove_leading_space/remove_leading_space.py
+++ remove_leading_space/remove_leading_space.py
@@ -5,8 +5,19 @@ Example:
 Input:         Juan Dela Cruz
 Output: Juan Dela Cruz
 """
+def valid_name(msg):
+    while True:
+        #remove the leading space
+        name = input(msg).strip()
+        #Make sure the name has a First and Last Name. 
+        #Also accepts names like Shaquille O'neal
+        if len(name) >= 3 and " " in name and \
+            all(char.isalpha() or char in ["'", " ","."] for char in name):
+            return name
+        else:
+            print("Invalid Name")
 
-#Enter the name and Remove the leading spaces
-name = input("Enter Name: ").strip()
+#Enter the name 
+name = valid_name("Enter Name: ")
 #print name
 print(name)
