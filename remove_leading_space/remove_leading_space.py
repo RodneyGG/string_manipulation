6834cd0bc79c3e277ba17cf63831dafd22a92488
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-19 00:24:55 +0800

    added an option to quit

--- remove_leading_space/remove_leading_space.py
+++ remove_leading_space/remove_leading_space.py
@@ -17,7 +17,22 @@ def valid_name(msg):
         else:
             print("Invalid Name")
 
-#Enter the name 
-name = valid_name("Enter Name: ")
-#print name
-print(name)
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
+
+try_again = True
+
+while try_again:
+    #Enter the name 
+    name = valid_name("Enter Name: ")
+    #print name
+    print(name)
+    
+    try_again = ask_quit()
