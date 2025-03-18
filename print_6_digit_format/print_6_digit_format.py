c4d74ca773bddeee8d5985cbcf2b20282701e88d
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-19 07:30:09 +0800

    Added a number validator

--- print_6_digit_format/print_6_digit_format.py
+++ print_6_digit_format/print_6_digit_format.py
@@ -6,8 +6,25 @@ Input: 143
 Output: 000143
 """
 
-#Ask the user to input a number 
-num = int(input("Enter a number")).strip()
-#convert the number into 6 digit format and print the number
-print(f"{num:06}")
+def valid_num(msg):
+    num = input(msg).strip()
+    try:
+        if float(num) == int(num):
+            return int(num)
+        else:     
+            return None
+    except ValueError:
+        print
 
+
+while True:
+    #Ask the user to input a number 
+    num = valid_num("Enter a number")
+    
+    if num is None:
+        print("This program only accept Whole numbers")
+    elif 0 <= num <= 1000: 
+        #convert the number into 6 digit format and print the number
+        print(f"{num:06}")
+    else:
+        print("Invalid Input")
