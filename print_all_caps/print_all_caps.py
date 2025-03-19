8431e0e08f78442c2ff6764bdf1fab5ec0bac138
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-19 08:02:36 +0800

    fixed all bugs and add option to quit

--- print_all_caps/print_all_caps.py
+++ print_all_caps/print_all_caps.py
@@ -10,13 +10,29 @@ def valid_name(msg):
     while True:
         name = input(msg)
         if len(name) >= 3 and " " in name and \
-            all(char.isalpha() or char ["'", ".", " "] for char in name):
+            all(char.isalpha() or char in ["'", " ", "."] for char in name):
             return name
         else:
             print("Invalid Name")
-        
-#Ask the user to input a name and Convert name to all uppercase
-name = input("Enter Name: ").upper().strip()
+            
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
+            
+try_again = True
 
-#print name
-print(name)
+while try_again:        
+    #Ask the user to input a name and Convert name to all uppercase
+    name = valid_name("Enter Name: ").upper().strip()
+
+    #print name
+    print(name)
+    
+    #ask user to quit
+    try_again = ask_quit()
