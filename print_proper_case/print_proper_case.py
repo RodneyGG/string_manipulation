b3c86a97ea8aad492df5e634f45f2a1ad7d5d9b4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-19 10:15:20 +0800

    Added a name validator

--- print_proper_case/print_proper_case.py
+++ print_proper_case/print_proper_case.py
@@ -5,9 +5,18 @@ Example:
 Input: jUAn DEla CrUZ
 Output: Juan Dela Cruz
 """
+def valid_name(msg):
+    while True:
+        name = input(msg).strip()
+        if len(name) >= 3 and " " in name \
+            and all(char.isalpha() or char in [" ", "'", "."] for char in name):
+            return name
+        else:
+            print("Invalid Input")
+
 
 #ask the user to input theri fullname in incorrect casing
-name = input("Enter Name in incorrect casing(jUAn DEla CrUZ):  ")
+name = valid_name("Enter Name in incorrect casing(jUAn DEla CrUZ):  ")
 #Convert in proper casing
 correct_casing = name.title()
 #print name
