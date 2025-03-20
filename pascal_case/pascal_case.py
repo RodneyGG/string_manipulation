ee213b3e7fc10ab5cb6219778105258edf9d67a8
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 10:46:29 +0800

    print pascal and fixed the pseudocode

--- pascal_case/pascal_case.py
+++ pascal_case/pascal_case.py
@@ -1,10 +1,17 @@
 """
-Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+Prog09: Create a program that ask the user to input their fullname in incorrect casing. 
+Print the input in pascal case.
 Example:
-Input: Juan Dela Cruz
-Output: 14
+Input: jUAn DEla CrUZ
+Output: JuanDelaCruz
 """
 
 #ask user to input their full name
+full_name = input("Enter Name: ")
 #convert it into pascal case
+pascal_case = []
+for name in full_name.split():
+    pascal_case.append(name.title())
 #print name in pascal case
+for word in pascal_case:
+    print(word, end= "")
