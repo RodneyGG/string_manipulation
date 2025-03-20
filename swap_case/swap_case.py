7421398760f09fa544d0aada91a0faab0a3132fd
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 09:26:43 +0800

    pseudocode

---
+++ swap_case/swap_case.py
@@ -0,0 +1,11 @@
+"""
+Prog06: Create a program that ask the user to input their fullname in incorrect casing. 
+Print each character of the input in reverse casing.
+Example:
+Input: jUAn DEla CrUZ
+Output: JuaN deLA cRuz
+"""
+
+#Ask the user to enter a name
+#Convert in reverse casing
+#print reverse case name
