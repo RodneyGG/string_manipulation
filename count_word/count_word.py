2545b44322fafd167e81d4904b1aa232b4ae91ef
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 09:42:57 +0800

    pseudocode

---
+++ count_word/count_word.py
@@ -0,0 +1,11 @@
+"""
+Prog07: Create a program that ask the user to input a complete statement. 
+Print the number of words in the input.
+Example:
+Input: We will weather the weather whatever the weather whether we like it or not
+Output: 14
+"""
+
+#Ask the user to input a sentence
+#count the words in the sentence
+#print the number of words in the sentence
