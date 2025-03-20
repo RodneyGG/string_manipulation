be2f2850fd223cc464b226e0153bf7dfbcbc8417
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 11:02:11 +0800

    added an option to quit

--- pascal_case/pascal_case.py
+++ pascal_case/pascal_case.py
@@ -6,6 +6,7 @@ Input: jUAn DEla CrUZ
 Output: JuanDelaCruz
 """
 
+# Name validator
 def valid_name(msg):
     while True:
         name = input(msg).strip()
@@ -15,12 +16,32 @@ def valid_name(msg):
         else:
             print("Enter Valid Full Name")
 
-#ask user to input their full name
-full_name = valid_name("Enter Name: ")
-#convert it into pascal case
-pascal_case = []
-for name in full_name.split():
-    pascal_case.append(name.title())
-#print name in pascal case
-for word in pascal_case:
-    print(word, end= "")
+#ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return 0
+        elif ask_user in ("n", "no"):
+            return 1
+        else:
+            print("Invalid Input. Only Y/N")
+
+try_again = True
+while try_again:        
+    #ask user to input their full name
+    full_name = valid_name("Enter Name: ")
+    
+    #convert it into pascal case
+    pascal_case = []
+    for name in full_name.split():
+        pascal_case.append(name.title())
+    
+    #print name in pascal case
+    for word in pascal_case:
+        print(word, end= "")
+    
+    #ask user to quit
+    try_again = ask_quit()
+    
+    
