302e4ae5ed564e11bc34653960cd733dea2cb814
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 10:10:18 +0800

    ppseudocode

---
+++ count_characters/count_characters.py
@@ -0,0 +1,11 @@
+"""
+Prog08: Create a program that ask the user to input their fullname. 
+Print the number of characters in the input.
+Example:
+Input: Juan Dela Cruz
+Output: 14
+"""
+
+#ask the user to enter their full name
+#count the characters in name
+#print the count
