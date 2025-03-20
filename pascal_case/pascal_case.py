c2ad2ef02f40b6298016875c8bd1467641a96c0c
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 10:56:44 +0800

    added a name validator

--- pascal_case/pascal_case.py
+++ pascal_case/pascal_case.py
@@ -6,8 +6,17 @@ Input: jUAn DEla CrUZ
 Output: JuanDelaCruz
 """
 
+def valid_name(msg):
+    while True:
+        name = input(msg).strip()
+        if len(name) >= 3 and " " in name and all(char.isalpha() \
+            or char in [" ", ".", "'"] for char in name):
+            return name
+        else:
+            print("Enter Valid Full Name")
+
 #ask user to input their full name
-full_name = input("Enter Name: ")
+full_name = valid_name("Enter Name: ")
 #convert it into pascal case
 pascal_case = []
 for name in full_name.split():
