0641b426fbc9ada133ea7f1a61d7d7d30ba5b167
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 10:01:53 +0800

    added an option to quit

--- count_word/count_word.py
+++ count_word/count_word.py
@@ -5,12 +5,27 @@ Example:
 Input: We will weather the weather whatever the weather whether we like it or not
 Output: 14
 """
+#ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return 0
+        elif ask_user in ("n", "no"):
+            return 1
+        else:
+            print("Invalid Input")
 
+try_again = True
 
-
-#Ask the user to input a word/s
-words = input("Enter a word/s: ")
-#count the words in the word/s
-count_word = words.split()
-#print the number of words in the word/s
-print(len(count_word))
+while try_again:
+    #Ask the user to input a word/s
+    words = input("Enter a word/s: ")
+    #count the words in the word/s
+    count_word = words.split()
+    #print the number of words in the word/s
+    print(len(count_word))
+    
+    #Ask user to quit
+    try_again = ask_quit()
+    
