8a03050582a06a2d893c682b5a59b89eac0e8ed4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 11:25:16 +0800

    print in snake case but has extra _

--- snake_case/snake_case.py
+++ snake_case/snake_case.py
@@ -7,6 +7,12 @@ Output: juan_dela_cruz
 """
 
 #Ask user to enter a name
+full_name =  input("Enter Name: ")
 #convert the name in snake case
+snake_case = []
+for name in full_name.split():
+    snake_case.append(name.lower())
 #print the name in the snake case
+for word in snake_case:
+    print(word, end= "_")
 
