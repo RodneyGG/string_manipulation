97b3b181a01a37b72bfa4dd3b9d623c120958608
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 09:38:18 +0800

    added an option to quit

--- swap_case/swap_case.py
+++ swap_case/swap_case.py
@@ -6,6 +6,7 @@ Input: jUAn DEla CrUZ
 Output: JuaN deLA cRuz
 """
 
+#Validate the name
 def valid_name(msg):
     while True:
         name = input(msg).strip()
@@ -13,13 +14,26 @@ def valid_name(msg):
             or char in ["'", ".", " "] for char in name):
             return name
         else:
-            print("Enter Valid Name")
+            print("Enter Valid Full Name")
 
+#Ask the user to quit           
+def ask_quit():
+    ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+    if ask_user in ("y", "yes"):
+        return 0
+    elif ask_user in ('n', "no"):
+        return 1
+    else:
+        print("Invalid Input. Only Y or N)")
 
+try_again = True
 
-#Ask the user to enter a name
-name = valid_name("Enter Name: ")
-#Convert in reverse casing
-reversed_name = name.swapcase()
-#print reverse case name
-print(reversed_name)
+while try_again:
+    #Ask the user to enter a name
+    name = valid_name("Enter Name: ")
+    #Convert in reverse casing
+    reversed_name = name.swapcase()
+    #print reverse case name
+    print(reversed_name)
+    
+    try_again = ask_quit()
