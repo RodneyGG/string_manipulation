7d629edc2f67117d3ef76a554858eeb6749da1d3
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 10:16:41 +0800

    added a name validator

--- count_characters/count_characters.py
+++ count_characters/count_characters.py
@@ -6,8 +6,18 @@ Input: Juan Dela Cruz
 Output: 14
 """
 
+def valid_name(msg):
+    while True:
+        name = input(msg).strip()
+        #Accept only if name has first and last name. 
+        if len(name) >= 3 and " " in name and all(char.isalpha() \
+            or char in [" ", ".", "'"] for char in name):
+            return name
+        else:
+            print("Invalid Name.")
+
 #ask the user to enter their full name
-name = input("Enter your Name: ")
+name = valid_name("Enter your Name: ")
 #count the characters in name
 count_characters = len(name)
 #print the count
