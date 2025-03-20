b7046e64d140efb2733a3d2bdf67c3329df896e0
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 10:38:55 +0800

    pseudo code

---
+++ pascal_case/pascal_case.py
@@ -0,0 +1,10 @@
+"""
+Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+Example:
+Input: Juan Dela Cruz
+Output: 14
+"""
+
+#ask user to input their full name
+#convert it into pascal case
+#print name in pascal case
