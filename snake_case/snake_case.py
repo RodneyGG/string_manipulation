1342dd05a2b61c13316273c423b6d30c1570dec4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 11:34:38 +0800

    added an option to quit

--- snake_case/snake_case.py
+++ snake_case/snake_case.py
@@ -15,16 +15,33 @@ def valid_name(msg):
             return name
         else:
             print("Enter Valid Full Name")
+            
+            
+#ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("\nDo you wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return 0
+        elif ask_user in ("n", "no"):
+            return 1
+        else:
+            print("Invalid Input. Only Y/N")
 
-#Ask user to enter a name
-full_name =  valid_name("Enter Name: ")
-#convert the name in snake case
-snake_case = []
-for name in full_name.split():
-    snake_case.append(name.lower())
-#print the name in the snake case
-for i in range(len(snake_case)):
-    if i == len(snake_case) - 1: 
-        print(snake_case[i], end="")
-    else:
-        print(snake_case[i], end="_")
+try_again = True
+
+while try_again:
+    #Ask user to enter a name
+    full_name =  valid_name("Enter Name: ")
+    #convert the name in snake case
+    snake_case = []
+    for name in full_name.split():
+        snake_case.append(name.lower())
+    #print the name in the snake case
+    for i in range(len(snake_case)):
+        if i == len(snake_case) - 1: 
+            print(snake_case[i], end="")
+        else:
+            print(snake_case[i], end="_")
+    
+    try_again = ask_quit()
