0a19cc4fddc01b6ff534b3a5213cec97bc6fc522
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-20 11:33:11 +0800

    added a name validator

--- snake_case/snake_case.py
+++ snake_case/snake_case.py
@@ -6,8 +6,18 @@ Input: jUAn DEla CrUZ
 Output: juan_dela_cruz
 """
 
+#Name validator
+def valid_name(msg):
+    while True:
+        name = input(msg).strip()
+        if len(name) >= 3 and " " in name and all(char.isalpha() \
+            or char in [" ", ".", "'"] for char in name):
+            return name
+        else:
+            print("Enter Valid Full Name")
+
 #Ask user to enter a name
-full_name =  input("Enter Name: ")
+full_name =  valid_name("Enter Name: ")
 #convert the name in snake case
 snake_case = []
 for name in full_name.split():
